072593caef777ee434ae923af4a147c683edc5f8
jbouny <jbouny@gmail.com>
2015-04-02 13:03:57 +0200

    Bypass all security constraints as cross-origin, same protocol, possibility to detect errors, with the default use of a proxy. Use a proxy instead of a direct iframe src, throw http://www.whateverorigin.org/ or Yahoo API.

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
-function handleIframeHeight() {
-  
-  var updateHeight = function updateHeight() {
-    var height = $( window ).height();
-    var iframe = $( '.colorblind' );
-    var iframeHeight = height - iframe.offset().top;
+var g_DomParser = new DOMParser(),
+    g_Container,
+    g_IframeErrorTimeout,
+    g_CurrentFilter;
     
-    iframe.css( 'height', iframeHeight + 'px' );
-  }
+function updateHeight() {
+  var height = $( window ).height();
+  var iframeHeight = height - g_Container.offset().top;
+  
+  g_Container.css( 'height', iframeHeight + 'px' );
+}
+
+function handleIframeHeight() {
   
   $( window ).resize( updateHeight );
   updateHeight();
@@ -28,21 +32,101 @@ function handleAbnormalityMenu() {
   $( '#abnormality-menu li a' ).click( function( event ) {
     event.preventDefault();
     
-    var key = $( this ).attr( 'key' );
-    loadFilter( key );
+    g_CurrentFilter = $( this ).attr( 'key' );
+    loadFilter();
     $( '#abnormality-menu .dropdown-toggle span' ).html( $( this ).html() );
   } ) ;
   
 }
 
-function loadFilter( filter ) {
+function loadFilter() {
   
-  var element = $( '.colorblind' );
-  var value = 'none' === filter ? '' : 'url(colorblindness.svg#' + filter + ')';
+  var value = 'none' === g_CurrentFilter ? '' : 'url(colorblindness.svg#' + g_CurrentFilter + ')';
   
-  element.css( 'filter', value );
-  element.css( '-webkit-filter', value );
+  g_Container.css( 'filter', value );
+  g_Container.css( '-webkit-filter', value );
   
+}
+
+function doAjax( url, container ) {
+
+  var urlParent = url;
+  if( url.indexOf( '://' ) !== -1 ) {
+  
+    var indexOfProtocol = url.indexOf( '://' ) + 3;
+    urlParent = url.substr( indexOfProtocol, url.length - indexOfProtocol );
+    if( urlParent.lastIndexOf( '/' ) !== -1 ) {
+      urlParent = urlParent.substr( 0, urlParent.lastIndexOf( '/' ) );
+    }
+    urlParent = url.substr( 0, indexOfProtocol ) + urlParent;
+  }
+    
+  var filterData = function filterData( data ) {
+  
+    data = data.replace( new RegExp('(href|src)="/', 'g' ),  '$1="'+urlParent+'/');
+    return data;
+    
+  }
+  
+  var setContent = function setContent( data ) {
+  
+    var doc = container[0];
+    
+    if( doc.contentDocument ) {
+      doc = doc.contentDocument;
+    }
+    else if( doc.contentWindow ) {
+      doc = doc.contentWindow.document;
+    }
+    
+    doc.open();
+    doc.writeln( data );
+    doc.close();
+    
+  };
+  
+	if( url.match( '^http' ) ) {
+    var content = '';
+    
+    var loadContentWithYahoo = function( url, successCallback, failCallback ) {
+    
+      var statement = 'select * from html where url="' + url + '" and xpath="//html"';
+      $.queryYQL( statement, 'xml', undefined, function ( data ) {
+        
+        if( data.results[0] ) {
+          successCallback( data.results[0] );
+        }
+        else {
+          failCallback();
+        }
+        
+      } );
+    }
+    
+    var loadContentWithWhateverOrigin = function( url, successCallback, failCallback ) {
+    
+      $.getJSON( 'http://whateverorigin.org/get?url=' + encodeURIComponent( url ) + '&callback=?', function( data ) {
+        successCallback( data.contents );
+      } );
+    
+    }
+    
+    var successCallback = function successCallback( data ) {
+      data = filterData( data );
+      setContent( data );
+    };
+    
+    var failCallback = function failCallback() {
+      var errormsg = '<p>Error: could not load the page.</p>';
+      setContent( errormsg );
+    };
+    
+    //loadContentWithYahoo( url, successCallback, failCallback );
+    loadContentWithWhateverOrigin( url, successCallback, failCallback );
+  }
+  else {
+    setContent( '<p>Error: Can only load http and https contents.</p>' );
+	}
 }
 
 function loadUrl( url ) {
@@ -51,8 +135,15 @@ function loadUrl( url ) {
     url = location.protocol + '//' + url;
   }
   
+  $( '#colorblind-container' ).html( '<iframe id="colorblind"></iframe>' );
+  g_Container = $( '#colorblind' );
+  updateHeight();
+  
+  loadFilter();
+  
   $( '#url-form .form-control' ).val( url );
-  $( 'iframe.colorblind' ).attr( 'src', url );
+  doAjax( url, g_Container );
+  //g_Container.attr( 'src', url );
   jump( url );
   
 }
@@ -66,12 +157,14 @@ function jump( anchor ) {
 }
 
 $( function main() {
+
+  g_Container = $( '#colorblind' );
+  g_CurrentFilter = 'none';
   
   handleIframeHeight();
   handleFormSubmit();
   handleAbnormalityMenu();
   
-  loadFilter( 'none' );
   loadUrl( 'https://duckduckgo.com' );
   
 } );
